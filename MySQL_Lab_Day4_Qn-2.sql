94a13f8c453d7abbdc14fc0c64453af6668583d5
Anjana P R <pranjana333@gmail.com>
2025-09-23 11:30:20 +0530

    Update Students table with additional data insertion and modify FeesStatus and FeesPaid for specific records

--- MySQL_Lab_Day4_Qn-2.sql
+++ MySQL_Lab_Day4_Qn-2.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS Students;
 CREATE TABLE Students (
     StudentID INT AUTO_INCREMENT PRIMARY KEY,
     StudentName VARCHAR(100) NOT NULL,
@@ -8,7 +9,27 @@ CREATE TABLE Students (
 
 INSERT INTO Students (StudentName, Course, FeesPaid, FeesStatus)
 VALUES
-('Alice', 'Web Development', 5000, 'Inactive')
+('Alice', 'Web Development', 5000, 'Inactive'),
 ('Bob', 'Data Science', 7000, 'Active'),
 ('Charlie', 'UI/UX Design', 4000, 'Active');
 
+SELECT * FROM Students
+WHERE FeesPaid > 5000;
+
+UPDATE Students
+SET FeesStatus = 'Active'
+WHERE Course = 'Web Development';
+
+UPDATE Students
+SET FeesPaid = FeesPaid + 1000
+WHERE Course = 'Data Science';
+
+UPDATE Students
+SET FeesStatus = "Inactive", FeesPaid = FeesPaid - 500
+WHERE StudentID = 3;
+
+DELETE FROM Students
+WHERE StudentID = 2;
+
+DELETE FROM Students
+WHERE FeesStatus = 'Inactive';  
