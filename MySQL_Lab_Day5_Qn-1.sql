54cd83f34fcaccb9e6df879d059829c4ab2fe972
Anjana P R <pranjana333@gmail.com>
2025-09-23 15:57:38 +0530

    Insert initial data into mobiles table

--- MySQL_Lab_Day5_Qn-1.sql
+++ MySQL_Lab_Day5_Qn-1.sql
@@ -5,4 +5,13 @@ CREATe TABLE mobiles(
     Price INT(10) NOT NULL,
     Stock INT(100) NOT NULL
 );
+INSERT INTO mobiles(ID, Brand, Model, Price, Stock)
 VALUES
+( 'Samsung', 'Galaxy M14', 12000, 30), 
+('Redmi', 'Note 12', 15000, 25), 
+('Realme', 'Narzo 50', 13000, 20),
+('Samsung', 'Galaxy A23', 18000, 10);
+
+SELECT * FROM mobiles 
+WHERE cost > 130000 OR stock < 15;
+
