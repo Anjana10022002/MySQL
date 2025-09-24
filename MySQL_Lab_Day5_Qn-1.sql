83349e5d0cb5fe68429d41e4cb80a8f9ea534b38
Anjana P R <pranjana333@gmail.com>
2025-09-24 12:33:07 +0530

    Fix SQL script for mobiles table: correct table creation, update queries, and ensure proper data handling

--- MySQL_Lab_Day5_Qn-1.sql
+++ MySQL_Lab_Day5_Qn-1.sql
@@ -1,17 +1,36 @@
+DROP TABLE IF EXISTS mobiles;
 CREATe TABLE mobiles(
     ID INT AUTO_INCREMENT PRIMARY KEY,
     Brand VARCHAR(50) NOT NULL,
     Model VARCHAR(50) NOT NULL,
-    Price INT(10) NOT NULL,
-    Stock INT(100) NOT NULL
+    Price INT NOT NULL,
+    Stock INT NOT NULL
 );
-INSERT INTO mobiles(ID, Brand, Model, Price, Stock)
+INSERT INTO mobiles(Brand, Model, Price, Stock)
 VALUES
-( 'Samsung', 'Galaxy M14', 12000, 30), 
+('Samsung', 'Galaxy M14', 12000, 30), 
 ('Redmi', 'Note 12', 15000, 25), 
 ('Realme', 'Narzo 50', 13000, 20),
 ('Samsung', 'Galaxy A23', 18000, 10);
 
 SELECT * FROM mobiles 
-WHERE cost > 130000 OR stock < 15;
+WHERE price > 13000 OR stock < 15;
+
+UPDATE mobiles
+SET stock = stock + 5, price = 12500
+WHERE Model = "Narzo 50"
+
+DELETE FROM mobiles
+WHERE ID = 2;
+
+SELECT MAX(price) AS HighestPrice, MIN(price) AS LowestPrice
+FROM mobiles;
+
+SELECT SUM(stock) AS TotalStock 
+FROM mobiles;
+
+SELECT * FROM mobiles
+ORDER BY price DESC
+LIMIT 2;
+
 
