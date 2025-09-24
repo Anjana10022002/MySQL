6aa71a947e27b20ce41844fb627187d7a4e0b07f
Anjana P R <pranjana333@gmail.com>
2025-09-24 12:15:51 +0530

    Fix table creation and data insertion for books; update queries for stock and price adjustments

--- MySQL_Lab_Day5_Qn-2.sql
+++ MySQL_Lab_Day5_Qn-2.sql
@@ -1,17 +1,32 @@
-CREATe TABLEbooks(
+DROP TABLE IF EXISTS books;
+CREATE TABLE books(
     ID INT AUTO_INCREMENT PRIMARY KEY,
     Title VARCHAR(100) NOT NULL,
     Author VARCHAR(50) NOT NULL,
-    Price INT(10) NOT NULL,
-    Stock INT(100) NOT NULL
+    Price INT NOT NULL,
+    Stock INT NOT NULL
 );
-INSERT INTO books(ID, Title, Author, Price, Stock)
+INSERT INTO books(Title, Author, Price, Stock)
 VALUES
-('The Alchemist', 'Paulo Coelho', 350, 50)
-('Atomic Habits', 'James Clear', 450, 40)
-('The Psychology of Money', 'Morgan Housel', 400, 30)
-('Ikigai', 'Francesc Miralles', 300, 60)
-('Deep Work', 'Cal Newport', 500, 20)
+('The Alchemist', 'Paulo Coelho', 350, 50),
+('Atomic Habits', 'James Clear', 450, 40),
+('The Psychology of Money', 'Morgan Housel', 400, 30),
+('Ikigai', 'Francesc Miralles', 300, 60),
+('Deep Work', 'Cal Newport', 500, 20);
 
 SELECT * FROM books 
-WHERE price > 450 OR stock < 30;
+WHERE price < 450 AND stock > 30;
+
+UPDATE books
+SET stock = 45, price = 420
+WHERE title = 'Deep Work';
+
+DELETE FROM books
+WHERE title = 'Ikigai';
+
+SELECT AVG(price) AS Average_Price, SUM(stock) AS Total_Stock
+FROM books;
+
+SELECT * FROM books
+ORDER BY price DESC
+LIMIT 3;
