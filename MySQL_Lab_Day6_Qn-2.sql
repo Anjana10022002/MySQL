e897c40b613d4aa8730133213c626a628bc9f29b
Anjana P R <pranjana333@gmail.com>
2025-09-25 12:17:50 +0530

    Fix SQL script for movies table: correct column name from Release_Date to Release_Year and update INSERT statement accordingly

--- MySQL_Lab_Day6_Qn-2.sql
+++ MySQL_Lab_Day6_Qn-2.sql
@@ -1,12 +1,13 @@
+DROP TABLE IF EXISTS movies;
 CREATE TABLE movies(
     ID INT AUTO_INCREMENT PRIMARY KEY,
     Title VARCHAR(50) NOT NULL,
     Genre VARCHAR(50) NOT NULL,
-    Release_Date INT(4) NOT NULL,
+    Release_Year INT(4) NOT NULL,
     Rating DECIMAL(3,1) NOT NULL,
     Box_Office INT NOT NULL
 );
-INSERT INTO movies(Title, Genre, Release_Date, Rating, Box_Office);
+INSERT INTO movies(Title, Genre, Release_Year, Rating, Box_Office)
 VALUES
 ('Inception', 'Sci-Fi', 2010, 8.8, 825),
 ('The Dark Knight', 'Action', 2008, 9.0, 1004),
@@ -18,3 +19,14 @@ SELECT Title, Box_Office AS `Earnings(in Cr)` From movies;
 
 SELECT Title AS `Movie Title`, Genre AS `Category`
 FROM movies;
+
+SELECT Title, Genre, Rating AS `IMDb Score`
+FROM movies
+WHERE Genre IN ('Sci-Fi', 'Action');
+
+SELECT Title, Release_Year AS `Release`
+FROM movies
+WHERE Release_Year BETWEEN 2008 AND 2015; 
+
+SELECT COUNT(*) AS High_Rated_Movies FROM movies
+WHERE Rating > 8.5;
