c7d4daeab96d9e28f4b220174d1ab377e8309ae9
Anjana P R <pranjana333@gmail.com>
2025-09-25 15:13:19 +0530

    Fix SQL script: correct DROP statement syntax and ensure proper table creation and data insertion for books1 and bestsellers

---
+++ MySQL_Lab_Day6_Qn-1.sql
@@ -0,0 +1,31 @@
+DROP TABLE IF EXISTs books1;
+DROP TABLE IF EXISTS bestsellers;
+CREATE TABLE books1(
+    ID INT NOT NULL PRIMARY KEY,
+    Title VARCHAR(100) NOT NULL,
+    Author VARCHAR(100) NOT NULL,
+    Genre VARCHAR(100) NOT NULL,
+    Price DECIMAL(10,2) NOT NULL,
+    Copies_Sold INT NOT NULL
+);
+INSERT INTO books1(ID,Title, Author, Genre, Price, Copies_Sold)
+VALUES
+(1, 'The Silent Patient', 'Alex Michaelides', 'Thriller', 399.00, 1200),
+(2, 'Atomic Habits', 'James Clear', 'Self-help', 499.00, 2000),
+(3, 'The Psychology of Money', 'Morgan Housel', 'Finance', 350.00, 1800);
+
+CREATE TABLE bestsellers(
+    ID INT NOT NULL PRIMARY KEY,
+    Title VARCHAR(100) NOT NULL,
+    Author VARCHAR(100) NOT NULL,
+    Genre VARCHAR(100) NOT NULL,
+    Price DECIMAL(10,2) NOT NULL,
+    Copies_Sold INT NOT NULL
+);
+INSERT INTO bestsellers(ID,Title, Author, Genre, Price, Copies_Sold)
+VALUES
+(4, 'Ikigai', 'Francesc Miralles', 'Philosophy', 300.00, 2500),
+(5, 'Think Like a Monk', 'Jay Shetty', 'Self-help', 450.00, 2200);
+
+SELECT * FROM books1;
+SELECT * FROM bestsellers;
