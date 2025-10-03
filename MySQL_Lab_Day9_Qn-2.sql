b5f07458d298ecde398dd14510f533c4e39f8be8
Anjana P R <pranjana333@gmail.com>
2025-10-03 22:40:37 +0530

    Refactor employees and departments table definitions to remove AUTO_INCREMENT from primary keys.

--- MySQL_Lab_Day9_Qn-2.sql
+++ MySQL_Lab_Day9_Qn-2.sql
@@ -1,9 +1,13 @@
 CREATE TABLE employees(
-    ID INT AUTO_INCREMENT PRIMARY KEY,
+    ID INT PRIMARY KEY,
     employeeName VARCHAR(50) NOT NULL
 );
-
+INSERT INTO employees(ID, employeeName)
+VALUES
+(1,"Anjali"),
+(2, "Rohan"),
+(3, "Meena");
 CREATE TABLE departments(
-    empID INT AUTO_INCREMENT PRIMARY KEY,
+    empID INT PRIMARY KEY,
     departmentName VARCHAR(50) NOT NULL
 );
