9e9292869a446ce8e35b4ff2412762a98925f322
Anjana P R <pranjana333@gmail.com>
2025-10-04 22:49:56 +0530

    Show the list of all books that have not been borrowed by anyone.

--- MySQL_Lab_Day9_Qn-1.sql
+++ MySQL_Lab_Day9_Qn-1.sql
@@ -26,3 +26,8 @@ ON books1.bookID = borrowers.bookID;
 SELECT borrowers.borrowerID, borrowers.borrowerName, books1.title FROM borrowers
 LEFT JOIN books1
 ON borrowers.bookID = books1.bookID;
+
+SELECT books1.bookID, books1.title FROM books1
+LEFT JOIN  borrowers
+ON books1.bookID = borrowers.bookID
+WHERE  borrowers.bookID IS NULL;
