ed9c1ef6eaedff0ab85d7d226929cadca1424016
Anjana P R <pranjana333@gmail.com>
2025-10-04 21:52:28 +0530

    created table named borrowers

--- MySQL_Lab_Day9_Qn-1.sql
+++ MySQL_Lab_Day9_Qn-1.sql
@@ -1,4 +1,16 @@
 CREATE  TABLE books(
-    bookID AUTO_INCREMENT PRIMARY KEY,
+    bookID INT PRIMARY KEY,
     Title VARCH(100) NOT NULL
 );
+INSERT INTO books
+VALUES
+(1, "The Alchemist"),
+(2, "The Power of Now"),
+(3, "Think and Grow Rich"),
+(4, "CLean Code");
+CREATE TABLE borrowers(
+    borrowerID INT PRIMARY KEY,
+    borrowerName VARCHAR(50) NOT NULL,
+    bookID INT
+);
+
