45eb1cc573ca50206afbc5dd7a5fe593ed4a5985
Anjana P R <pranjana333@gmail.com>
2025-10-08 22:00:45 +0530

    insert values to category table

--- MySQL_Lab_Day11_Qn-2.sql
+++ MySQL_Lab_Day11_Qn-2.sql
@@ -2,6 +2,11 @@ CREATE TABLE categories (
     categoryID INT AUTO_INCREMENT PRIMARY KEY,
     categoryName VARCHAR(100) NOT NULL
 );
+INSERT INTO categories (categoryName)
+VALUES 
+('Fiction'),
+('Science'),
+('History');
 CREATE TABLE books2 (
     bookID INT AUTO_INCREMENT PRIMARY KEY,
     bookName VARCHAR(100) NOT NULL,
