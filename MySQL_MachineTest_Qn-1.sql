a2590c23d6e558a7f43d001458254d48bb59c4b8
Anjana P R <pranjana333@gmail.com>
2025-10-09 22:47:15 +0530

    created table called exam

--- MySQL_MachineTest_Qn-1.sql
+++ MySQL_MachineTest_Qn-1.sql
@@ -3,4 +3,9 @@ CREATE TABLE employee(
     employeeName VARCHAR(100) NOT NULL,
     department VARCHAR(100) NOT NULL,
     leave INT NOT NULL 
-)
+);
+CREATE TABLE exam(
+    ID INT AUTO_INCREMENT,
+    employeeID INT NOT NULL,
+    examStatus VARCHAR(20)
+);
