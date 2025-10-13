d9bdd94273a1a019bf1f9ddb242efbc88a4f3d7c
Anjana P R <pranjana333@gmail.com>
2025-10-13 23:41:12 +0530

    Insert values into book table.

--- MySQL_Lab_Day11_Qn-1.sql
+++ MySQL_Lab_Day11_Qn-1.sql
@@ -5,10 +5,19 @@ CREATE TABLE authors(
 INSERT INTO authors(authorName) 
 VALUES 
 ('Author 1'), ('Author 2'), ('Author 3');
+
 CREATE TABLE book(
     bookID INT AUTO_INCREMENT PRIMARY KEY,
     bookTitle VARCHAR(100) NOT NULL,
     authorID INT,
     FOREIGN KEY(authorID) REFERENCES authors(authorID)
 );
+INSERT INTO book(bookTitle, authorID) 
+VALUES 
+('Book A', 1), 
+('Book B', 2), 
+('Book C', 1), 
+('Book D', 3), 
+('Book E', 2);
+
 CREATE INDEX idx_author_id ON book(authorID);
