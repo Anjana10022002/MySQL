2bc3127d88586faf554e5aaacee21c38c19bbd9c
Anjana P R <pranjana333@gmail.com>
2025-10-20 23:17:09 +0530

    fixing syntaax error

--- MySQL_Lab_Day10_Qn-1.sql
+++ MySQL_Lab_Day10_Qn-1.sql
@@ -3,10 +3,12 @@ CREATE TABLE students(
     studentName VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL
 );
+
 CREATE TABLE courses(
     courseID INT PRIMARY KEY AUTO_INCREMENT,
     courseName VARCHAR(100) NOT NULL
 );
+
 CREATE TABLE enrollments(
     studentID INT,
     courseID INT,
